1fb41893b4eb87474ee47fd7def05969b817f96f
Foucault-Lapeze <115037697+Foucault-Lapeze@users.noreply.github.com>
2025-11-06 15:27:52 +0100

    add database

---
+++ ProjetDotNet/Models/TodoContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetDotNet.Models;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<ToDoItem> TodoItems { get; set; }
+}
